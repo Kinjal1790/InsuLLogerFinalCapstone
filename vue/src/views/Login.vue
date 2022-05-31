48a8e833ae7478b5169fa8d2dd57845f7af1a9dc
Kinjalben Jivani <jivani.kinjal92@gmail.com>
2022-05-31 11:12:31 -0500

    login.vue changed

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -6,7 +6,7 @@
         class="alert alert-danger"
         role="alert"
         v-if="invalidCredentials"
-      >Invalid username and password!</div>
+      >Invalid email and password!</div>
       <div
         class="alert alert-success"
         role="alert"
