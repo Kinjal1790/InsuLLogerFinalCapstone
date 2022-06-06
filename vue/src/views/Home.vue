fa8b8c2ef276646e6bba1ec8d1ec161a0adad518
Andrii Kostenko <kostenkoandrii@gmail.com>
2022-06-06 11:05:52 -0400

    minor changes to home and settings

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -3,11 +3,11 @@
     <div class="home-info">
       <h1>Welcome to iTracker</h1>
       <h3>Tracking your blood sugar levels just got a whole lot easier</h3>
-      <p>InsuLoggerr is a multipurpose tool that helps you maintain a healthy lifestyle with diabetes. 
+      <p>InsuLogger is a multipurpose tool that helps you maintain a healthy lifestyle with diabetes. 
         With InsuLogger’s easy-to-use features, you can monitor your basal blood sugar level and calculate bolus insulin doses so you can worry less about your blood sugar, and focus more on doing what you love. </p>
       <p>The longer you use InsuLogger, the better it becomes. Users who regularly input their carbohydrate (carb) intake are able to view 3-day, two-week, and monthly reports that show averages of their  blood sugar and bolus insulin doses compared to target ranges. Our warning and alert notifications will let you know when your blood sugar is out of healthy range so you can take action and stay on track. Click on the login page to create an account and start tracking!</p>
       <button @click='displayLogin()' v-if="$store.state.token == ''">Login</button>
-      <button v-if="$store.state.token != ''" @click='$router.push("/bolus")'>Input</button>
+      <button v-if="$store.state.token != ''" @click='$router.push("/bolus")'>Generate Bolus</button>
     </div>
     <div class="side-img">
       <img src="../style/4435922.jpg" alt="a woman measuring her blood sugar level">
