062a9646ebb2642d9560b04e21a7ec4d68cbdeee
Eric Gonzalez <Ericgonzalez1108@gmail.com>
2022-06-07 10:27:25 -0400

    csv export for report

--- vue/src/views/Report.vue
+++ vue/src/views/Report.vue
@@ -49,6 +49,7 @@
                 </tr>
             </tbody>
         </table>
+        <button v-on:click="exportTableToCSV('report.csv')">Export</button>
     </div>
    
 
@@ -93,6 +94,47 @@ export default {
                 }
 
             })
+        },
+        downloadCSV(csv, filename) {
+    let csvFile;
+    let downloadLink;
+
+    // CSV file
+    csvFile = new Blob([csv], {type: "text/csv"});
+
+    // Download link
+    downloadLink = document.createElement("a");
+
+    // File name
+    downloadLink.download = filename;
+
+    // Create a link to the file
+    downloadLink.href = window.URL.createObjectURL(csvFile);
+
+    // Hide download link
+    downloadLink.style.display = "none";
+
+    // Add the link to DOM
+    document.body.appendChild(downloadLink);
+
+    // Click download link
+    downloadLink.click();
+        },
+        exportTableToCSV(filename) {
+    let csv = [];
+    let rows = document.querySelectorAll("table tr");
+    
+    for (let i = 0; i < rows.length; i++) {
+        var row = [], cols = rows[i].querySelectorAll("td, th");
+        
+        for (let j = 0; j < cols.length; j++) 
+            row.push(cols[j].innerText);
+        
+        csv.push(row.join(","));        
+    }
+
+    // Download CSV file
+            this.downloadCSV(csv.join("\n"), filename);
         }
 
     }
